eeb5dd367f43a558b9046469e5e88481fdc0c154
Dawn <brenner95670@gmail.com>
2019-10-06 00:16:56 -0700

    changed requirements.txt

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -1,9 +1,7 @@
 function buildMetadata(sample) {
 
-  // THIS FUNCTION WORKS!
-
-  // Use `d3.json` to fetch the metadata for a sample
-  // Use d3 to select the panel with id of `#sample-metadata`
+// Use `d3.json` to fetch the metadata for a sample
+// Use d3 to select the panel with id of `#sample-metadata`
   d3.json(`/metadata/${sample}`).then((data) => {
     var panel = d3.select("#sample-metadata");
 
@@ -11,7 +9,6 @@ function buildMetadata(sample) {
     panel.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
     Object.entries(data).forEach(([key, value]) => {
       panel.append("p").text(`${key}: ${value}`);
@@ -23,16 +20,9 @@ function buildMetadata(sample) {
 
 function buildCharts(sample) {
 
-
-  //THIS IS THE BUBBLE CHART.  I WASN'T SURE IF I SHOULD KEEP USING THE SAME
-  //NAMES FOR THINGS SO I USED 'RESPONSE' AND 'BUBBLE' - BUT NOT SURE IF I DID IT
-  //CORRECTLY.  ALSO, IT SEEMS LIKE THE BUBBLE SIZE SHOULD BE ON THE "COUNT"
-  //OF SAMPLE VALUES, NOT SAMPLE VALUES, BUT THE EXAMPLE PHOTO LOOKS LIKE
-  //IT WAS JUST DON'T ON THE VALUE.
-
-  //   // @TODO: Use `d3.json` to fetch the sample data for the plots
+//  Use `d3.json` to fetch the sample data for the plots
   d3.json(`/samples/${sample}`).then((response) => {
-    //   // @TODO: Build a Bubble Chart using the sample data
+//  Build a Bubble Chart using the sample data
     var trace1 = {
       x: response.otu_ids,
       y: response.sample_values,
@@ -47,24 +37,31 @@ function buildCharts(sample) {
     var bubble = [trace1];
 
     var layout = {
-      title: 'Bubble Chart',
       showlegend: false,
-      plot_bgcolor:"lightgray",
-      paper_bgcolor:"#FFF3",
       height: innerHeight,
-      width: innerWidth
+      width: innerWidth,
+      yaxis: {
+        title: {
+          text: 'Samples'
+        }
+      },
+      xaxis: {
+        title: {
+          text: 'OTU ID'
+          // font: {
+          //   family: 'Courier New, monospace',
+          //   size: 14,
+          //   color: '#7f7f7f'
+        }
+      }
     };
 
     Plotly.newPlot('bubble', bubble, layout, { responsive: true });
   });
 
-  //THIS IS THE PIE CHART. AGAIN, I WASN'T SURE IF I SHOULD REUSE VARIABLES,
-  // SO I CALLED THINGS 'RESPONSE' AND 'PIEINFO.'  NOT SURE IF I DID THE SLICE
-  // IN THE CORRECT PLACE
 
-  //   // @TODO: Build a Pie Chart
-  //   // HINT: You will need to use slice() to grab the top 10 sample_values,
-  //   // otu_ids, and labels (10 each).
+    // Build a Pie Chart
+    // Use slice() to grab the top 10 sample_value
   d3.json(`/samples/${sample}`).then((response) => {
     var pieInfo = [{
       values: response.sample_values.slice(0, 10),
@@ -82,17 +79,13 @@ function buildCharts(sample) {
         b: 10,
         t: 1,
         pad: 1
-    }
-      
+      }
+
     };
 
     Plotly.newPlot('pie', pieInfo, layout, { displayModeBar: false }, { responsive: true });
   });
 };
-
-//THIS PART IS WORKING FOR THE SELECT SAMPLE AND METADATA DISPLAY, BUT
-//I THINK I MESSED UP CLOSING [],(), OR {}. AND NOT SURE IF THE LAST LINE,
-//"INIT()" GOES INSIDE OR OUTSIDE THE LAST BRACKETS.
 
 function init() {
   // Grab a reference to the dropdown select element
